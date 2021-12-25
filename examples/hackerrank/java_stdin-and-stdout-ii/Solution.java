48aea21b503d1792058215613e8704df6d64f36c
Suilan Maia <suilansmaia@gmail.comn>
2021-12-25 18:50:51 -0300

    hackerrank - java stdin and stdout exercise complete

--- examples/hackerrank/java_stdin-and-stdout-ii/Solution.java
+++ examples/hackerrank/java_stdin-and-stdout-ii/Solution.java
@@ -3,10 +3,17 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
+        // https://docs.oracle.com/javase/7/docs/api/java/util/Scanner.html
         Scanner scan = new Scanner(System.in);
+        
+        // change delimeter to new line 
+        scan.useDelimiter("[;\r\n]+");
         int i = scan.nextInt();
 
         // Write your code here.
+        double d = scan.nextDouble();
+
+        String s = scan.next();
 
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
